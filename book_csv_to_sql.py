5ad485cac9ade7c2024a119a943c9d711b5adbc3
Aelly-A <aellyalwardi@gmail.com>
2025-06-04 22:28:38 -0700

    add autoloader

--- book_csv_to_sql.py
+++ book_csv_to_sql.py
@@ -4,26 +4,34 @@ from random import randint
 csv = "Books.csv"
 df = read_csv(csv, dtype={'Book-Author': str}, engine='python')
 columns = ["ISBN", "Book-Title", "Book-Author", "Year-Of-Publication", "Publisher", "Checked-Out"]
+used_isbns = []
 
 with open("books.sql", "w") as file:
-    file.write("CREATE TABLE Books(\n"
-               "\tisbn VARCHAR(16) PRIMARY KEY,\n"
-               "\ttitle VARCHAR(256) NOT NULL,\n"
-               "\tauthor VARCHAR(128),\n"
-               "\tpublication_year int(4),\n"
-               "\tpublisher VARCHAR(128),\n"
-               "\ttotal_num_at_branch INTEGER\n"
-               ");\n\n")
+    file.write("DROP TABLE IF EXISTS Book;\n")
+    file.write("DROP TABLE IF EXISTS User;\n")
+    file.write("DROP TABLE IF EXISTS Waitlist;\n")
+    file.write("DROP TABLE IF EXISTS Loan;\n")
+    file.write("DROP TABLE IF EXISTS LoanHistory;\n")
+
+    file.write("CREATE TABLE Book("
+               "isbn VARCHAR(16) PRIMARY KEY,"
+               "title VARCHAR(256) NOT NULL,"
+               "author VARCHAR(128),"
+               "publication_year int(4),"
+               "publisher VARCHAR(128),"
+               "total_num_at_branch INTEGER"
+               ");\n")
 
     for index, row in df.iterrows():
         if isna(row["Book-Author"]) or isna(row["Publisher"]) or isna(row["Year-Of-Publication"]):
             continue
 
         isbn = row['ISBN']
-        title = row['Book-Title'].replace("'", "\\'")
-        author = row['Book-Author'].replace("'", "\\'")
+
+        title = row['Book-Title'].replace("'", "\\'").replace('"', '\\"')
+        author = row['Book-Author'].replace("'", "\\'").replace('"', '\\"')
         publication_year = row['Year-Of-Publication']
-        publisher = row['Publisher'].replace("'", "\\'")
+        publisher = row['Publisher'].replace("'", "\\'").replace('"', '\\"')
         total_num_at_branch = randint(1, 10)
 
-        file.write(f"INSERT INTO Books (isbn, title, author, publication_year, publisher, total_num_at_branch) VALUES ('{isbn}', '{title}', '{author}', {publication_year}, '{publisher}', {total_num_at_branch});\n")
+        file.write(f"INSERT INTO Book (isbn, title, author, publication_year, publisher, total_num_at_branch) VALUES ('{isbn}', '{title}', '{author}', {publication_year}, '{publisher}', {total_num_at_branch});\n")
---
+++ load_db.py
@@ -0,0 +1,19 @@
+from mysql.connector import connect
+
+if __name__ == "__main__":
+    username = input("Enter your SQL username: ")
+    password = input("Enter your SQL password: ")
+    port = input("Enter your SQL port (press enter for the default, 3306): ")
+
+    if port == "":
+        port = "3306"
+
+    conn = connect(user=username, password=password, host="localhost", database="library",
+                   collation='utf8mb4_unicode_ci', port=port)
+
+    cur = conn.cursor()
+
+    for line in open("books.sql", "r"):
+        cur.execute(line)
+
+    conn.close()
